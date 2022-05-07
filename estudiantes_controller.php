742e5a47d601d0d6e524632fd8bcc5aaa20dd8d3
Cebans <101933183+Cebans@users.noreply.github.com>
2022-05-07 18:00:23 -0500

    estudiantes controller

---
+++ estudiantes_controller.php
@@ -0,0 +1,30 @@
+<?php
+namespace controllers;
+
+use controllers\IController;
+use db\ConexionDB;
+use models\Estudiante;
+
+class EstudianteController implements IController
+{
+
+    public function list()
+    {
+    }
+
+    public function detail($id)
+    {
+    }
+
+    public function create($estudianteModel)
+    {
+    }
+
+    public function update($d,$estudaiantesModel)
+    {
+    }
+
+    public function delete($sid)
+    {
+    }
+}
